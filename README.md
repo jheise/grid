ba79d3bfb8e9ce886531cc67caae76a3a8630183
Jon Heise <j.heise@gmail.com>
2014-04-28 21:30:33 -0700

    adding README

---
+++ README.md
@@ -0,0 +1,3 @@
+Grid is a 3d engine written in C++ Python and OpenGL 3.1.
+
+Grid uses boost python to allow python scripts to drive events within the engine.
--- scripts/grid.py
+++ scripts/grid.py
@@ -3,3 +3,5 @@ from time import time,ctime
 print "Today is", ctime(time())
 print "Creating TARDIS"
 factory.create_object("../models/TARDIS", "TARDIS.dae")
+#print "Creating 343 Guilty Spark"
+#factory.create_object("../models/spark", "skpfile.dae")
