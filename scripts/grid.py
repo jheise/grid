c3740007688cf736b76e1d818ee9dd7bc22d5620
Jon Heise <j.heise@gmail.com>
2014-05-07 00:57:23 -0700

    reseting script

--- scripts/grid.py
+++ scripts/grid.py
@@ -1,7 +1,7 @@
 from time import time,ctime
 
 print "Today is", ctime(time())
-#print "Creating TARDIS"
-#factory.create_object("../models/TARDIS", "TARDIS.dae")
+print "Creating TARDIS"
+factory.create_object("../models/TARDIS", "TARDIS.dae")
 #print "Creating 343 Guilty Spark"
 #factory.create_object("../models/spark", "skpfile.dae")
